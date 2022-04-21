fd5513822f9ff52b32f86104c87ac957d3f17964
agnobayiS <C.ya@live.com>
2022-04-21 16:08:25 +0200

    third commit

--- test/greet.tests.js
+++ test/greet.tests.js
@@ -1,6 +1,42 @@
 console.log(greet("Bob"));
 console.log(greet("sam"));
 
+
+var itemList = [
+    {name : 'apples', qty : 10},
+    {name : 'pears', qty : 37},
+    {name : 'bananas', qty : 27},
+    {name : 'apples', qty : 3},
+  ];
+  
+  var results = [
+    {name : 'pears', qty : 37},
+    {name : 'bananas', qty : 27},
+  ];
+  
+  var itemList2 = [
+    {name : 'apples', qty : 10},
+    {name : 'pears', qty : 19},
+    {name : 'bananas', qty : 17},
+    {name : 'apples', qty : 3},
+  ];
+  
+  var results2 = [];
+  
+  var itemList3 = [
+    {name : 'apples', qty : 40},
+    {name : 'pears', qty : 20},
+    {name : 'bananas', qty : 23},
+    {name : 'apples', qty : 37}
+  ];
+  
+  var results3 = [
+    {name : 'apples', qty : 40},
+    {name : 'bananas', qty : 23},
+    {name : 'apples', qty : 37}
+  ];
+  
+
 describe('this test (greet)' , function(){
     it('should return hello and a name that is given' , function(){
         assert.equal('Hello, boo',greet('boo'));
