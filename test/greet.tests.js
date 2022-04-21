35007fcf9d25b85e87a2cfdc97770020f36f0ef4
agnobayiS <C.ya@live.com>
2022-04-21 16:13:02 +0200

    forth commit

--- test/greet.tests.js
+++ test/greet.tests.js
@@ -1,41 +1,3 @@
-console.log(greet("Bob"));
-console.log(greet("sam"));
-
-
-var itemList = [
-    {name : 'apples', qty : 10},
-    {name : 'pears', qty : 37},
-    {name : 'bananas', qty : 27},
-    {name : 'apples', qty : 3},
-  ];
-  
-  var results = [
-    {name : 'pears', qty : 37},
-    {name : 'bananas', qty : 27},
-  ];
-  
-  var itemList2 = [
-    {name : 'apples', qty : 10},
-    {name : 'pears', qty : 19},
-    {name : 'bananas', qty : 17},
-    {name : 'apples', qty : 3},
-  ];
-  
-  var results2 = [];
-  
-  var itemList3 = [
-    {name : 'apples', qty : 40},
-    {name : 'pears', qty : 20},
-    {name : 'bananas', qty : 23},
-    {name : 'apples', qty : 37}
-  ];
-  
-  var results3 = [
-    {name : 'apples', qty : 40},
-    {name : 'bananas', qty : 23},
-    {name : 'apples', qty : 37}
-  ];
-  
 
 describe('this test (greet)' , function(){
     it('should return hello and a name that is given' , function(){
@@ -45,12 +7,12 @@ describe('this test (greet)' , function(){
     });
 
     it('should return hello even if you enter a number' , function(){
-        assert.equal("Hello, sam",greet('sam'));
+        assert.equal("Hello, 2",greet('2'));
 
 
     });
-    it('should return hello even if you enter a number' , function(){
-        assert.equal("Hello, ",greet(""));
+    it('should return hello even if you enter an empty string' , function(){
+        assert.equal("Hello, 2",greet("2"));
 
 
     });
