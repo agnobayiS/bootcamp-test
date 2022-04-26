a4f0398b0fbbfa56484ca05b094166d726da2d63
agnobayiS <C.ya@live.com>
2022-04-26 10:15:15 +0200

    second commit

--- test/countRegNumber.tests.js
+++ test/countRegNumber.tests.js
@@ -6,11 +6,11 @@ describe('this test (countRegNumber)' , function(){
     });
 
     it('should return one when the is an empty string' , function(){
-        assert.equal(1,countRegNumber(''));
+        assert.equal(1,countRegNumber(""));
 
     //     assert.deepEqual([2,2],[2,2]);
     });
-    it('should return the number the number of regNo in the list' , function(){
+    it('should return the total number of registration numbers in the list' , function(){
         assert.equal(3,countRegNumber("111 CY,233 CY,222 CY"));
 
         // assert.deepEqual([2,2],[2,2]);
--- test/greet.tests.js
+++ test/greet.tests.js
@@ -7,12 +7,12 @@ describe('this test (greet)' , function(){
     });
 
     it('should return hello even if you enter a number' , function(){
-        assert.equal("Hello, 2",greet('2'));
+        assert.equal("Hello, 2",greet(2));
 
 
     });
     it('should return hello even if you enter an empty string' , function(){
-        assert.equal("Hello, 2",greet("2"));
+        assert.equal("Hello, ",greet(""));
 
 
     });
